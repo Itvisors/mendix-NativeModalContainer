127ff3bb160a78adba72f61f711c824da427e92c
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2020-05-17 08:52:21 +0200

    Initial commit

---
+++ src/NativeModalContainer.jsx
@@ -0,0 +1,48 @@
+import { Component, createElement } from "react";
+
+import { Dimensions, Modal, View } from "react-native";
+
+export class NativeModalContainer extends Component {
+    state = {
+        windowWidth: Dimensions.get("window").width,
+        windowHeight: Dimensions.get("window").height
+    };
+    render() {
+        const { content, modalVisible } = this.props;
+        if (!modalVisible || modalVisible.status !== "available") {
+            return null;
+        }
+
+        const style = {
+            flexDirection: "row",
+            flex: 1
+        };
+        return (
+            <Modal visible={modalVisible.value} transparent={true}>
+                <View style={style}>{content}</View>
+            </Modal>
+        );
+    }
+
+    handler = newDimensions => {
+        // console.info(
+        //     "NativeImageViewer dimension change handler, new width: " +
+        //         newDimensions.window.width +
+        //         ", height: " +
+        //         newDimensions.window.height
+        // );
+        this.setState({
+            windowHeight: newDimensions.window.height,
+            windowWidth: newDimensions.window.width
+        });
+    };
+
+    componentDidMount() {
+        Dimensions.addEventListener("change", this.handler);
+    }
+
+    componentWillUnmount() {
+        // Important to stop updating state after unmount
+        Dimensions.removeEventListener("change", this.handler);
+    }
+}
