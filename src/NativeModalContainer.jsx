ec47039a7e39897b414d907332f41e87cb1ae220
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2022-04-10 14:30:40 +0200

    Widget as function component

--- src/NativeModalContainer.jsx
+++ src/NativeModalContainer.jsx
@@ -1,48 +1,29 @@
-import { Component, createElement } from "react";
+import { Modal, View } from "react-native";
+import { createElement } from "react";
 
-import { Dimensions, Modal, View } from "react-native";
+export function NativeModalContainer({ content, modalVisible }) {
+    if (!modalVisible) {
+        return null;
+    }
 
-export class NativeModalContainer extends Component {
-    state = {
-        windowWidth: Dimensions.get("window").width,
-        windowHeight: Dimensions.get("window").height
+    const style = {
+        flexDirection: "row",
+        flex: 1
     };
-    render() {
-        const { content, modalVisible } = this.props;
-        if (!modalVisible || modalVisible.status !== "available") {
-            return null;
-        }
-
-        const style = {
-            flexDirection: "row",
-            flex: 1
-        };
-        return (
-            <Modal visible={modalVisible.value} transparent={true}>
-                <View style={style}>{content}</View>
-            </Modal>
-        );
-    }
-
-    handler = newDimensions => {
-        // console.info(
-        //     "NativeModalContainer dimension change handler, new width: " +
-        //         newDimensions.window.width +
-        //         ", height: " +
-        //         newDimensions.window.height
-        // );
-        this.setState({
-            windowHeight: newDimensions.window.height,
-            windowWidth: newDimensions.window.width
-        });
-    };
-
-    componentDidMount() {
-        Dimensions.addEventListener("change", this.handler);
-    }
-
-    componentWillUnmount() {
-        // Important to stop updating state after unmount
-        Dimensions.removeEventListener("change", this.handler);
-    }
+    return (
+        <Modal
+            visible={!!modalVisible.value}
+            transparent={true}
+            style={{ flex: 1 }}
+            supportedOrientations={[
+                "portrait",
+                "portrait-upside-down",
+                "landscape",
+                "landscape-left",
+                "landscape-right"
+            ]}
+        >
+            <View style={style}>{content}</View>
+        </Modal>
+    );
 }
